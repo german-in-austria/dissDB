ec6ce2b01e21d266b5c8ff1eec432cefc78b4442
hcb79 <hcb@hcb.de>
2018-01-06 19:49:19 +0100

    AnnotationsDB - Models update.

--- app/AnnotationsDB/models.py
+++ app/AnnotationsDB/models.py
@@ -14,17 +14,17 @@ class event(models.Model):
 		ordering = ('layer',)
 
 class token(models.Model):
-	text				= models.CharField(max_length=511			, blank=True, null=True									, verbose_name="Das aktuelle Token Wort")
+	text				= models.CharField(max_length=511																	, verbose_name="Das aktuelle Token")
 	token_type_id		= models.ForeignKey('token_type'			, blank=True, null=True	, on_delete=models.SET_NULL		, verbose_name="Token Type")
 	ortho				= models.CharField(max_length=511			, blank=True, null=True									, verbose_name="Ortho")
 	ID_Inf				= models.ForeignKey('Datenbank.Informanten'	, blank=True, null=True	, on_delete=models.SET_NULL		, verbose_name="ID Informant")
 	fragment_of			= models.ForeignKey('token', related_name='rn_token_fragment_of', blank=True, null=True	, on_delete=models.SET_NULL	, verbose_name="Fragment von")
-	token_id			= models.ForeignKey('token', related_name='rn_token_token_id', blank=True, null=True	, on_delete=models.SET_NULL	, verbose_name="Token ID")
+	token_reihung		= models.IntegerField(						  null=True												verbose_name="Token Reihung")
 	event_id			= models.ForeignKey('event', related_name='rn_token_event_id', blank=True, null=True	, on_delete=models.SET_NULL	, verbose_name="Event ID")
 	start_timepoint		= models.DurationField(																				  verbose_name="Start Zeitpunkt")
 	end_timepoint		= models.DurationField(																				  verbose_name="End Zeitpunkt")
 	transcript_id		= models.ForeignKey('transcript'			, blank=True, null=True	, on_delete=models.SET_NULL		, verbose_name="Transcript ID")
-	likely_error		= models.BooleanField(default=False																	, verbose_name="likely Fehler")
+	likely_error		= models.BooleanField(default=False																	, verbose_name="Eventueller Fehler")
 	sentence_id			= models.ForeignKey('Datenbank.Saetze'		, blank=True, null=True	, on_delete=models.SET_NULL		, verbose_name="Sentence ID")
 	sequence_in_sentence= models.IntegerField(						  null=True												, verbose_name="sequence_in_sentence")
 	text_in_ortho		= models.CharField(max_length=511			, blank=True, null=True									, verbose_name="Text in Ortho")
@@ -34,10 +34,10 @@ class token(models.Model):
 		db_table = "token"
 		verbose_name = "Token"
 		verbose_name_plural = "Tokens"
-		ordering = ('sequence_in_sentence',)
+		ordering = ('sentence_id','token_reihung',)
 
 class token_type(models.Model):
-	token_type_name		= models.CharField(max_length=511			, blank=True, null=True									, verbose_name="Token Typ Name")
+	token_type_name		= models.CharField(max_length=511																	, verbose_name="Token Typ Name")
 	def __str__(self):
 		return "{}".format(self.token_type_name)
 	class Meta:
