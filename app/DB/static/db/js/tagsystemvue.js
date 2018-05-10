2bd1706d0887d0507c3f629c454d189ab2ce032d
hcb79 <hcb@hcb.de>
2018-05-10 12:29:04 +0200

    AnnotationsDB - Start (VUE) - Audioplayer ausgelagert und Tag-System als VUE-component vorbereitet.

---
+++ app/DB/static/db/js/tagsystemvue.js
@@ -0,0 +1,19 @@
+/* global Vue */
+
+Vue.component('tagsystem', {
+	delimiters: ['${', '}'],
+	template: '#tagsystem-template',
+	props: [],
+	data: function () {
+		return {
+		};
+	},
+	computed: {
+	},
+	methods: {
+	},
+	mounted: function () {
+	},
+	beforeDestroy: function () {
+	}
+});
