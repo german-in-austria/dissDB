d5b7fc6d68d31edfe515a38b5743f49b2376266a
hcb79 <hcb@hcb.de>
2019-10-25 12:43:30 +0200

    Anno-Check-> Filter Vorbereitet.

--- app/AnnotationsDB/views_annocheck.py
+++ app/AnnotationsDB/views_annocheck.py
@@ -35,175 +35,222 @@ def views_annocheck(request):
 	# Basisdaten für Filter laden
 	if 'getBaseData' in request.POST:
 		return httpOutput(json.dumps({'OK': True}, 'application/json'))
+	# Filter Daten ausgeben
+	if 'getFilterData' in request.POST:
+		aFilter = json.loads(request.POST.get('filter'))
+		aAntwortenElement = dbmodels.Antworten.objects.all()
+		aShowCount = True if request.POST.get('showCount') == "true" else False
+		showCountTrans = True if aShowCount and request.POST.get('showCountTrans') == "true" else False
+		# Tag Ebenen ermitteln
+		# ToDo: Antworten Filter hinzufügen
+		nTagEbenen = {}
+		aTagEbenen = [{'pk': 0, 'title': 'Alle', 'count': aAntwortenElement.count() if aShowCount else -1}]
+		for aTE in dbmodels.TagEbene.objects.all():
+			nTagEbenen[aTE.pk] = str(aTE)
+			aTagEbenen.append({'pk': aTE.pk, 'title': str(aTE), 'count': aAntwortenElement.filter(
+				antwortentags__id_TagEbene_id=aTE.pk
+			).distinct().count() if aShowCount else -1})
+		# Informanten ermitteln
+		# ToDo: Antworten Filter hinzufügen
+		aInformanten = [{'pk': 0, 'kuerzelAnonym': 'Alle', 'count': aAntwortenElement.count() if aShowCount else -1}]
+		for aInf in dbmodels.Informanten.objects.all():
+			aInformanten.append({'pk': aInf.pk, 'kuerzelAnonym': aInf.Kuerzel_anonym, 'count': aAntwortenElement.filter(
+				von_Inf_id=aInf.pk
+			).distinct().count() if aShowCount else -1})
+		# Transkripte ermitteln
+		# ToDo: Antworten Filter hinzufügen
+		aTranskripte = [{'pk': 0, 'name': 'Alle', 'count': aAntwortenElement.count() if aShowCount else -1}]
+		aTranskripte.append({'pk': -1, 'name': 'Keine Transkripte', 'count': aAntwortenElement.filter(
+			ist_token=None,
+			ist_tokenset=None
+		).distinct().count() if aShowCount else -1})
+		aTranskripte.append({'pk': -2, 'name': 'Nur Transkripte', 'count': aAntwortenElement.filter(
+			Q(ist_token__gt=0) |
+			Q(ist_tokenset__gt=0)
+		).distinct().count() if aShowCount else -1})
+		for aTrans in adbmodels.transcript.objects.all():
+			aTranskripte.append({'pk': aTrans.pk, 'name': aTrans.name, 'count': aAntwortenElement.filter(
+				Q(ist_token__gt=0) |
+				Q(ist_tokenset__gt=0),
+				Q(ist_token__transcript_id_id=aTrans.pk) |
+				Q(ist_tokenset__id_von_token__transcript_id_id=aTrans.pk) |
+				Q(ist_tokenset__tbl_tokentoset__id_token__transcript_id_id=aTrans.pk)
+			).distinct().count() if aShowCount and showCountTrans else -1})
+		return httpOutput(json.dumps({'OK': True, 'tagEbenen': aTagEbenen, 'informanten': aInformanten, 'transcripts': aTranskripte}), 'application/json')
 	# Einträge auslesen
 	if 'getEntries' in request.POST:
-		aSeite = int(request.POST.get('seite'))
-		aEps = int(request.POST.get('eps'))
+		aSeite = int(request.POST.get('seite')) if request.POST.get('seite') else 0
+		aEps = int(request.POST.get('eps')) if request.POST.get('eps') else 0
 		aFilter = json.loads(request.POST.get('filter'))
 		aSuche = json.loads(request.POST.get('suche')) if request.POST.get('suche') else []
-		aSortierung = json.loads(request.POST.get('sortierung'))
-		aElemente = adbmodels.mat_adhocsentences.objects.all()
+		aSortierung = json.loads(request.POST.get('sortierung')) if request.POST.get('sortierung') else []
+		aAntwortenElement = dbmodels.Antworten.objects.all()
 		# Suchen / Filtern
-		aSucheMuss = []
-		aSucheKann = []
-		if int(aFilter['trans']) > 0:
-			aSucheMuss.append(Q(transid=aFilter['trans']))
-		if int(aFilter['inf']) > 0:
-			aSucheMuss.append(Q(infid=aFilter['inf']))
-		# [{'value': 'zwei', 'kannmuss': 'kann', 'methode': 'ci', 'name': 'sentorig'}, {'value': '', 'kannmuss': 'kann', 'methode': 'ci', 'name': 'sentorth'}, {'value': '', 'kannmuss': 'kann', 'methode': 'ci', 'name': 'sentttpos'}, {'value': '', 'kannmuss': 'kann', 'methode': 'ci', 'name': 'sentsptag'}]
-		for aSuchFeld in aSuche:
-			if aSuchFeld['value'].strip():
-				aSuchValue = aSuchFeld['value'].strip()
-				if 'regex' in aSuchFeld['methode']:
-					aTyp = aSuchFeld['methode']
-					aSuchValue = r"{0}".format(aSuchValue)
-				else:
-					aTyp = 'icontains' if aSuchFeld['methode'] == 'ci' else 'contains'
-				print(aSuchFeld['methode'], aTyp)
-				if aSuchFeld['kannmuss'] == 'muss':
-					aSucheMuss.append(Q(**{aSuchFeld['name'] + '__' + aTyp: aSuchValue}))
-				if aSuchFeld['kannmuss'] == 'nicht':
-					aSucheMuss.append(~Q(**{aSuchFeld['name'] + '__' + aTyp: aSuchValue}))
-				if aSuchFeld['kannmuss'] == 'kann':
-					aSucheKann.append(Q(**{aSuchFeld['name'] + '__' + aTyp: aSuchValue}))
-		if aSucheMuss:
-			import operator
-			aSucheMussX = aSucheMuss[0]
-			for aMuss in aSucheMuss[1:]:
-				aSucheMussX = operator.and_(aSucheMussX, aMuss)
-		if aSucheKann:
-			import operator
-			aSucheKannX = aSucheKann[0]
-			for aMuss in aSucheKann[1:]:
-				aSucheKannX = operator.or_(aSucheKannX, aMuss)
-		if aSucheMuss and aSucheKann:
-			aElemente = aElemente.filter(aSucheMussX, aSucheKannX)
-		elif aSucheMuss:
-			aElemente = aElemente.filter(aSucheMussX)
-		elif aSucheKann:
-			aElemente = aElemente.filter(aSucheKannX)
-		# Sortieren
-		aElemente = aElemente.order_by(('-' if not aSortierung['asc'] else '') + aSortierung['spalte'])
+		# Einträge laden
 		# Einträge ausgeben
-		aMatIds = [aEintrag['id'] for aEintrag in aElemente.values('id')[aSeite * aEps:aSeite * aEps + aEps]]
-		aEintraege = [
-			{
-				'adhoc_sentence': aEintrag.adhoc_sentence,
-				'tokenids': aEintrag.tokenids,
-				'tokens': aEintrag.tokens,
-				'infid': aEintrag.infid,
-				'transid': aEintrag.transid,
-				'tokreih': aEintrag.tokreih,
-				'seqsent': aEintrag.seqsent,
-				'sentorig': aEintrag.sentorig,
-				'sentorth': aEintrag.sentorth,
-				'left_context': aEintrag.left_context,
-				'senttext': aEintrag.senttext,
-				'right_context': aEintrag.right_context,
-				'sentttlemma': aEintrag.sentttlemma,
-				'sentttpos': aEintrag.sentttpos,
-				'sentsplemma': aEintrag.sentsplemma,
-				'sentsppos': aEintrag.sentsppos,
-				'sentsptag': aEintrag.sentsptag,
-				'sentspdep': aEintrag.sentspdep,
-				'sentspenttype': aEintrag.sentspenttype
-			}
-			for aEintrag in adbmodels.mat_adhocsentences.objects.raw('''
-				SELECT "mat_adhocsentences".*,
-					(
-						SELECT array_to_json(array_agg(row_to_json(atok)))
-						FROM (
-							SELECT "token".*,
-							(
-								SELECT array_to_json(array_agg(row_to_json(aantwort)))
-									FROM (
-										SELECT "Antworten".*,
-										(
-											SELECT array_to_json(array_agg(row_to_json(aAntwortenTags)))
-												FROM (
-													SELECT "AntwortenTags".*
-													FROM "AntwortenTags"
-													WHERE "AntwortenTags"."id_Antwort_id" = "Antworten"."id"
-													ORDER BY "AntwortenTags"."id_TagEbene_id" ASC, "AntwortenTags"."Reihung" ASC
-												) AS aAntwortenTags
-										) AS AntwortenTags_raw
-										FROM "Antworten"
-										WHERE "Antworten"."ist_token_id" = "token"."id"
-									) AS aantwort
-							) AS antworten,
-							(
-								SELECT array_to_json(array_agg(row_to_json(atokenset)))
-									FROM (
-											SELECT "tokenset".*,
-											(
-												SELECT array_to_json(array_agg(row_to_json(aantwort)))
-													FROM (
-														SELECT "Antworten".*,
-														(
-															SELECT array_to_json(array_agg(row_to_json(aAntwortenTags)))
-																FROM (
-																	SELECT "AntwortenTags".*
-																	FROM "AntwortenTags"
-																	WHERE "AntwortenTags"."id_Antwort_id" = "Antworten"."id"
-																	ORDER BY "AntwortenTags"."id_TagEbene_id" ASC, "AntwortenTags"."Reihung" ASC
-																) AS aAntwortenTags
-														) AS AntwortenTags_raw
-														FROM "Antworten"
-														WHERE "Antworten"."ist_tokenset_id" = "tokenset"."id"
-													) AS aantwort
-											) AS antworten,
-											(
-												SELECT array_to_json(array_agg(row_to_json(atokentoset_cache)))
-													FROM (
-														SELECT "tokentoset"."id_token_id"
-														FROM "tokentoset"
-														WHERE "tokentoset"."id_tokenset_id" = "tokenset"."id"
-													) AS atokentoset_cache
-											) AS tokentoset
-												FROM "tokenset"
-												LEFT OUTER JOIN "tokentoset" ON ( "tokenset"."id" = "tokentoset"."id_tokenset_id" )
-												WHERE "tokentoset"."id_token_id" = "token"."id"
-										UNION ALL
-											SELECT "tokenset".*,
-											(
-												SELECT array_to_json(array_agg(row_to_json(aantwort)))
-													FROM (
-														SELECT "Antworten".*,
-														(
-															SELECT array_to_json(array_agg(row_to_json(aAntwortenTags)))
-																FROM (
-																	SELECT "AntwortenTags".*
-																	FROM "AntwortenTags"
-																	WHERE "AntwortenTags"."id_Antwort_id" = "Antworten"."id"
-																	ORDER BY "AntwortenTags"."id_TagEbene_id" ASC, "AntwortenTags"."Reihung" ASC
-																) AS aAntwortenTags
-														) AS AntwortenTags_raw
-														FROM "Antworten"
-														WHERE "Antworten"."ist_tokenset_id" = "tokenset"."id"
-													) AS aantwort
-											) AS antworten,
-											(
-												SELECT array_to_json(array_agg(row_to_json(atokentoset_cache)))
-													FROM (
-														SELECT "tokentoset_cache"."id_token_id"
-														FROM "tokentoset_cache"
-														WHERE "tokentoset_cache"."id_tokenset_id" = "tokenset"."id"
-													) AS atokentoset_cache
-											) AS tokentoset
-												FROM "tokenset"
-												LEFT OUTER JOIN "tokentoset_cache" ON ( "tokenset"."id" = "tokentoset_cache"."id_tokenset_id" )
-												WHERE "tokentoset_cache"."id_token_id" = "token"."id"
-									) AS atokenset
-							) AS tokensets
-								FROM "token"
-								WHERE "token"."id" = ANY("mat_adhocsentences"."tokenids")
-								ORDER BY "token"."token_reihung" ASC
-						) atok
-					) AS "tokens"
-				FROM "mat_adhocsentences"
-				WHERE "mat_adhocsentences"."id" IN %s
-				ORDER BY "mat_adhocsentences"."adhoc_sentence" ASC
-			''', [tuple(aMatIds)])]
-		# print(connection.queries)
-		return httpOutput(json.dumps({'OK': True, 'seite': aSeite, 'eps': aEps, 'eintraege': aEintraege, 'zaehler': aElemente.count()}), 'application/json')
+		return httpOutput(json.dumps({'OK': True, 'seite': aSeite, 'eps': aEps, 'eintraege': [{'test': 'test'}], 'zaehler': aAntwortenElement.count()}), 'application/json')
+		# aElemente = adbmodels.mat_adhocsentences.objects.all()
+		# # Suchen / Filtern
+		# aSucheMuss = []
+		# aSucheKann = []
+		# if int(aFilter['trans']) > 0:
+		# 	aSucheMuss.append(Q(transid=aFilter['trans']))
+		# if int(aFilter['inf']) > 0:
+		# 	aSucheMuss.append(Q(infid=aFilter['inf']))
+		# # [{'value': 'zwei', 'kannmuss': 'kann', 'methode': 'ci', 'name': 'sentorig'}, {'value': '', 'kannmuss': 'kann', 'methode': 'ci', 'name': 'sentorth'}, {'value': '', 'kannmuss': 'kann', 'methode': 'ci', 'name': 'sentttpos'}, {'value': '', 'kannmuss': 'kann', 'methode': 'ci', 'name': 'sentsptag'}]
+		# for aSuchFeld in aSuche:
+		# 	if aSuchFeld['value'].strip():
+		# 		aSuchValue = aSuchFeld['value'].strip()
+		# 		if 'regex' in aSuchFeld['methode']:
+		# 			aTyp = aSuchFeld['methode']
+		# 			aSuchValue = r"{0}".format(aSuchValue)
+		# 		else:
+		# 			aTyp = 'icontains' if aSuchFeld['methode'] == 'ci' else 'contains'
+		# 		print(aSuchFeld['methode'], aTyp)
+		# 		if aSuchFeld['kannmuss'] == 'muss':
+		# 			aSucheMuss.append(Q(**{aSuchFeld['name'] + '__' + aTyp: aSuchValue}))
+		# 		if aSuchFeld['kannmuss'] == 'nicht':
+		# 			aSucheMuss.append(~Q(**{aSuchFeld['name'] + '__' + aTyp: aSuchValue}))
+		# 		if aSuchFeld['kannmuss'] == 'kann':
+		# 			aSucheKann.append(Q(**{aSuchFeld['name'] + '__' + aTyp: aSuchValue}))
+		# if aSucheMuss:
+		# 	import operator
+		# 	aSucheMussX = aSucheMuss[0]
+		# 	for aMuss in aSucheMuss[1:]:
+		# 		aSucheMussX = operator.and_(aSucheMussX, aMuss)
+		# if aSucheKann:
+		# 	import operator
+		# 	aSucheKannX = aSucheKann[0]
+		# 	for aMuss in aSucheKann[1:]:
+		# 		aSucheKannX = operator.or_(aSucheKannX, aMuss)
+		# if aSucheMuss and aSucheKann:
+		# 	aElemente = aElemente.filter(aSucheMussX, aSucheKannX)
+		# elif aSucheMuss:
+		# 	aElemente = aElemente.filter(aSucheMussX)
+		# elif aSucheKann:
+		# 	aElemente = aElemente.filter(aSucheKannX)
+		# # Sortieren
+		# aElemente = aElemente.order_by(('-' if not aSortierung['asc'] else '') + aSortierung['spalte'])
+		# # Einträge ausgeben
+		# aMatIds = [aEintrag['id'] for aEintrag in aElemente.values('id')[aSeite * aEps:aSeite * aEps + aEps]]
+		# aEintraege = [
+		# 	{
+		# 		'adhoc_sentence': aEintrag.adhoc_sentence,
+		# 		'tokenids': aEintrag.tokenids,
+		# 		'tokens': aEintrag.tokens,
+		# 		'infid': aEintrag.infid,
+		# 		'transid': aEintrag.transid,
+		# 		'tokreih': aEintrag.tokreih,
+		# 		'seqsent': aEintrag.seqsent,
+		# 		'sentorig': aEintrag.sentorig,
+		# 		'sentorth': aEintrag.sentorth,
+		# 		'left_context': aEintrag.left_context,
+		# 		'senttext': aEintrag.senttext,
+		# 		'right_context': aEintrag.right_context,
+		# 		'sentttlemma': aEintrag.sentttlemma,
+		# 		'sentttpos': aEintrag.sentttpos,
+		# 		'sentsplemma': aEintrag.sentsplemma,
+		# 		'sentsppos': aEintrag.sentsppos,
+		# 		'sentsptag': aEintrag.sentsptag,
+		# 		'sentspdep': aEintrag.sentspdep,
+		# 		'sentspenttype': aEintrag.sentspenttype
+		# 	}
+		# 	for aEintrag in adbmodels.mat_adhocsentences.objects.raw('''
+		# 		SELECT "mat_adhocsentences".*,
+		# 			(
+		# 				SELECT array_to_json(array_agg(row_to_json(atok)))
+		# 				FROM (
+		# 					SELECT "token".*,
+		# 					(
+		# 						SELECT array_to_json(array_agg(row_to_json(aantwort)))
+		# 							FROM (
+		# 								SELECT "Antworten".*,
+		# 								(
+		# 									SELECT array_to_json(array_agg(row_to_json(aAntwortenTags)))
+		# 										FROM (
+		# 											SELECT "AntwortenTags".*
+		# 											FROM "AntwortenTags"
+		# 											WHERE "AntwortenTags"."id_Antwort_id" = "Antworten"."id"
+		# 											ORDER BY "AntwortenTags"."id_TagEbene_id" ASC, "AntwortenTags"."Reihung" ASC
+		# 										) AS aAntwortenTags
+		# 								) AS AntwortenTags_raw
+		# 								FROM "Antworten"
+		# 								WHERE "Antworten"."ist_token_id" = "token"."id"
+		# 							) AS aantwort
+		# 					) AS antworten,
+		# 					(
+		# 						SELECT array_to_json(array_agg(row_to_json(atokenset)))
+		# 							FROM (
+		# 									SELECT "tokenset".*,
+		# 									(
+		# 										SELECT array_to_json(array_agg(row_to_json(aantwort)))
+		# 											FROM (
+		# 												SELECT "Antworten".*,
+		# 												(
+		# 													SELECT array_to_json(array_agg(row_to_json(aAntwortenTags)))
+		# 														FROM (
+		# 															SELECT "AntwortenTags".*
+		# 															FROM "AntwortenTags"
+		# 															WHERE "AntwortenTags"."id_Antwort_id" = "Antworten"."id"
+		# 															ORDER BY "AntwortenTags"."id_TagEbene_id" ASC, "AntwortenTags"."Reihung" ASC
+		# 														) AS aAntwortenTags
+		# 												) AS AntwortenTags_raw
+		# 												FROM "Antworten"
+		# 												WHERE "Antworten"."ist_tokenset_id" = "tokenset"."id"
+		# 											) AS aantwort
+		# 									) AS antworten,
+		# 									(
+		# 										SELECT array_to_json(array_agg(row_to_json(atokentoset_cache)))
+		# 											FROM (
+		# 												SELECT "tokentoset"."id_token_id"
+		# 												FROM "tokentoset"
+		# 												WHERE "tokentoset"."id_tokenset_id" = "tokenset"."id"
+		# 											) AS atokentoset_cache
+		# 									) AS tokentoset
+		# 										FROM "tokenset"
+		# 										LEFT OUTER JOIN "tokentoset" ON ( "tokenset"."id" = "tokentoset"."id_tokenset_id" )
+		# 										WHERE "tokentoset"."id_token_id" = "token"."id"
+		# 								UNION ALL
+		# 									SELECT "tokenset".*,
+		# 									(
+		# 										SELECT array_to_json(array_agg(row_to_json(aantwort)))
+		# 											FROM (
+		# 												SELECT "Antworten".*,
+		# 												(
+		# 													SELECT array_to_json(array_agg(row_to_json(aAntwortenTags)))
+		# 														FROM (
+		# 															SELECT "AntwortenTags".*
+		# 															FROM "AntwortenTags"
+		# 															WHERE "AntwortenTags"."id_Antwort_id" = "Antworten"."id"
+		# 															ORDER BY "AntwortenTags"."id_TagEbene_id" ASC, "AntwortenTags"."Reihung" ASC
+		# 														) AS aAntwortenTags
+		# 												) AS AntwortenTags_raw
+		# 												FROM "Antworten"
+		# 												WHERE "Antworten"."ist_tokenset_id" = "tokenset"."id"
+		# 											) AS aantwort
+		# 									) AS antworten,
+		# 									(
+		# 										SELECT array_to_json(array_agg(row_to_json(atokentoset_cache)))
+		# 											FROM (
+		# 												SELECT "tokentoset_cache"."id_token_id"
+		# 												FROM "tokentoset_cache"
+		# 												WHERE "tokentoset_cache"."id_tokenset_id" = "tokenset"."id"
+		# 											) AS atokentoset_cache
+		# 									) AS tokentoset
+		# 										FROM "tokenset"
+		# 										LEFT OUTER JOIN "tokentoset_cache" ON ( "tokenset"."id" = "tokentoset_cache"."id_tokenset_id" )
+		# 										WHERE "tokentoset_cache"."id_token_id" = "token"."id"
+		# 							) AS atokenset
+		# 					) AS tokensets
+		# 						FROM "token"
+		# 						WHERE "token"."id" = ANY("mat_adhocsentences"."tokenids")
+		# 						ORDER BY "token"."token_reihung" ASC
+		# 				) atok
+		# 			) AS "tokens"
+		# 		FROM "mat_adhocsentences"
+		# 		WHERE "mat_adhocsentences"."id" IN %s
+		# 		ORDER BY "mat_adhocsentences"."adhoc_sentence" ASC
+		# 	''', [tuple(aMatIds)])]
+		# # print(connection.queries)
+		# return httpOutput(json.dumps({'OK': True, 'seite': aSeite, 'eps': aEps, 'eintraege': aEintraege, 'zaehler': aElemente.count()}), 'application/json')
 	return render_to_response('AnnotationsDB/annocheck.html', RequestContext(request))
--- webpack_src/annoCheck/src/components/Filtern.vue
+++ webpack_src/annoCheck/src/components/Filtern.vue
@@ -4,8 +4,11 @@
       <label for="tagebene" class="col-sm-4 control-label">Tag Ebene</label>
       <div class="col-sm-8">
         <select class="form-control" v-model="filterfelder.tagebene" id="tagebene">
-          <option value="0">Auswählen</option>
-          <option :value="tagebene.pk" v-for="tagebene in tagebenenListe" :key="'te' + tagebene">{{ tagebene.t }}</option>
+          <option :value="tagebene.pk"
+            v-for="tagebene in tagebenenListe"
+            :key="'te' + tagebene.pk"
+            :disabled="tagebene.count == 0"
+          >{{ tagebene.title }}{{ tagebene.count > -1 ? ' - ' + tagebene.count.toLocaleString() : '' }}</option>
         </select>
       </div>
     </div>
@@ -13,8 +16,11 @@
       <label for="informant" class="col-sm-4 control-label">Informant</label>
       <div class="col-sm-8">
         <select class="form-control" v-model="filterfelder.informant" id="informant">
-          <option value="0">Alle</option>
-          <option :value="informant.pk" v-for="informant in infTransListFiltered" :key="'inf' + informant">{{informant.modelStr}} - {{informant.transcriptsPKs.length}} Transkripte</option>
+          <option :value="informant.pk"
+            v-for="informant in informantenListe"
+            :key="'inf' + informant.pk"
+            :disabled="informant.count == 0"
+          >{{informant.kuerzelAnonym}}{{ informant.count > -1 ? ' - ' + informant.count.toLocaleString() : '' }}</option>
         </select>
       </div>
     </div>
@@ -22,12 +28,31 @@
       <label for="transkript" class="col-sm-4 control-label">Transkript</label>
       <div class="col-sm-8">
         <select class="form-control" v-model="filterfelder.transkript" id="transkript">
-          <option value="0">Alle</option>
-          <option :value="transcript.pk" v-for="transcript in transcriptsListFiltered" :key="'ts' + transcript">{{transcript.name}}</option>
+          <option :value="transcript.pk"
+            v-for="transcript in transcriptsListe"
+            :key="'ts' + transcript.pk"
+            :disabled="transcript.count == 0"
+          >{{transcript.name}}{{ transcript.count > -1 ? ' - ' + transcript.count.toLocaleString() : '' }}</option>
         </select>
       </div>
     </div>
     <div class="form-group">
+      <div class="col-sm-offset-4 col-sm-8">
+        <div class="form-inline">
+          <div class="checkbox">
+            <label>
+              <input type="checkbox" v-model="showCount"> Zähler anzeigen
+            </label>
+          </div>
+          <div class="checkbox">
+            <label class="ml10">
+              <input type="checkbox" v-model="showCountTrans" :disabled="!showCount"> Transkript
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- <div class="form-group">
       <label for="aufgabensets" class="col-sm-4 control-label">Aufgabensets</label>
       <div class="col-sm-8">
         <select class="form-control" id="aufgabensets">
@@ -42,7 +67,7 @@
           <option value="0">Alle</option>
         </select>
       </div>
-    </div>
+    </div> -->
     <div class="loading" v-if="loading">Lade ...<div>{{ loadInfos }}</div></div>
   </div>
 </template>
@@ -56,76 +81,48 @@ export default {
       loading: false,
       loadInfos: '',
       mvDurchschnitt: 0.0,
-      mvLastUpdate: 'Unbekannt ...'
+      mvLastUpdate: 'Unbekannt ...',
+      tagebenenListe: [],
+      informantenListe: [],
+      transcriptsListe: [],
+      showCount: true,
+      showCountTrans: false
     }
   },
   computed: {
-    tagebenenListe () {
-      return this.tagsData.data.ready ? this.tagsData.data.baseCache.tagebenenList : []
-    },
-    infTransListFiltered () {
-      if (this.filterfelder.transkript > 0) {
-        let infTransList = []
-        this.infTrans.data.infTransList.forEach(informant => {
-          if (informant.transcriptsPKs.indexOf(this.filterfelder.transkript) > -1) {
-            infTransList.push(informant)
-          }
-        }, this)
-        return infTransList
-      }
-      return this.infTrans.data.infTransList
-    },
-    transcriptsListFiltered () {
-      if (this.filterfelder.informant > 0) {
-        let transcriptsList = []
-        this.infTrans.data.transcriptsList.forEach(transcript => {
-          if (this.infTrans.data.infTransObj[this.filterfelder.informant].transcriptsPKs.indexOf(transcript.pk) > -1) {
-            transcriptsList.push(transcript)
-          }
-        }, this)
-        return transcriptsList
-      }
-      return this.infTrans.data.transcriptsList
-    }
   },
   mounted () {
     console.log(this.filterfelder)
-    this.getTranscriptsInfList()
+    this.getFilterData()
   },
   methods: {
-    getTranscriptsInfList () {    // Informationen zu Informanten und Transkripten laden
+    getFilterData () {    // Informationen für Filter laden
       this.loading = true
-      this.loadInfos = ''
-      this.http.post('/annotationsdb/startvue', { getTranscriptsInfList: 1 })
-        .then((response) => {
-          this.infTrans.data.infTransList = response.data['informanten']
-          this.infTrans.data.infTransObj = {}
-          for (let aInfKey in this.infTrans.data.infTransList) {
-            this.infTrans.data.infTransList[aInfKey].transcriptsPKs = this.infTrans.data.infTransList[aInfKey].transcriptsPKs.filter(function (el) { return el != null })   // Null Werte filtern!
-            // Objekt mit PK als Property erstellen
-            if (this.infTrans.data.infTransList.hasOwnProperty(aInfKey)) {
-              let aInf = this.infTrans.data.infTransList[aInfKey]
-              this.infTrans.data.infTransObj[aInf.pk] = aInf
-            }
-          }
-          this.infTrans.data.transcriptsList = response.data['transcripts']
-          this.infTrans.data.transcriptsObj = {}
-          for (let aTransKey in this.infTrans.data.transcriptsList) {
-            // Objekt mit PK als Property erstellen
-            if (this.infTrans.data.transcriptsList.hasOwnProperty(aTransKey)) {
-              let aTrans = this.infTrans.data.transcriptsList[aTransKey]
-              this.infTrans.data.transcriptsObj[aTrans.pk] = aTrans
-            }
-          }
-          this.infTrans.data.loaded = true
-          this.loading = false
-        })
-        .catch((err) => {
-          console.log(err)
-          alert('Fehler!')
-          this.loading = false
-        })
+      this.loadInfos = 'Filter Daten'
+      this.http.post('', {
+        getFilterData: true,
+        showCount: this.showCount,
+        showCountTrans: this.showCountTrans,
+        filter: JSON.stringify({ inf: this.filterfelder.informant, trans: this.filterfelder.transkript })
+      }).then((response) => {
+        this.tagebenenListe = response.data['tagEbenen']
+        this.informantenListe = response.data['informanten']
+        this.transcriptsListe = response.data['transcripts']
+        console.log('getFilterData', response.data)
+        this.loading = false
+      }).catch((err) => {
+        console.log(err)
+        alert('Fehler!')
+        this.loading = false
+      })
     }
+  },
+  watch: {
+    'filterfelder.tagebene' () { this.getFilterData() },
+    'filterfelder.informant' () { this.getFilterData() },
+    'filterfelder.transkript' () { this.getFilterData() },
+    showCount () { this.getFilterData() },
+    showCountTrans () { this.getFilterData() }
   }
 }
 </script>
